11e316670709fb1f7425b2f82e4d51e6f11a4dc4
helahelahela <hmakovic@tvz.hr>
2021-11-18 17:17:55 +0100

    Create Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="info">
+      <div id="user">
+        <div class="slika"></div>
+        <h1>Username</h1>
+      </div>
+
+      <div id="nav">
+        <ul>
+          <li><router-link to="/">Timeline</router-link></li>
+          <li><router-link to="/Calendar">Calendar</router-link></li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<style>
+</style>
+
+<script>
+</script>
