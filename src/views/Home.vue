1bd31114cc882c2d49d8921dec96f2516e4da3c3
helahelahela <hmakovic@tvz.hr>
2021-11-18 17:18:05 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,94 @@
 <template>
   <div class="home">
+    <app-header></app-header>
+
+    <app-content>
+    <div id="display">
+      <div class="article">
+        <h2>Sretan dan</h2>
+        <p>
+          May indulgence difficulty ham can put especially. Bringing remember
+          for supplied her why was confined. Middleton principle did she
+          procuring extensive believing add. Weather adapted prepare oh is
+          calling. These wrong of he which there smile to my front. He fruit oh
+          enjoy it of whose table. Cultivated occasional old her unpleasing
+          unpleasant. At as do be against pasture covered viewing started.
+          Enjoyed me settled mr respect no spirits civilly.
+        </p>
+        <br />
+        <p>
+          Material confined likewise it humanity raillery an unpacked as he.
+          Three chief merit no if. Now how her edward engage not horses. Oh
+          resolution he dissimilar precaution to comparison an. Matters engaged
+          between he of pursuit manners we moments. Merit gay end sight front.
+          Manor equal it on again ye folly by match. In so melancholy as an
+          sentiments simplicity connection. Far supply depart branch agreed old
+          get our.
+        </p>
+        <p class="date">9.10.2021.</p>
+        <hr />
+      </div>
+
+      <div class="article">
+        <h2>Dosadan dan</h2>
+        <p>
+          Prevailed sincerity behaviour to so do principle mr. As departure at
+          no propriety zealously my. On dear rent if girl view. First on smart
+          there he sense. Earnestly enjoyment her you resources. Brother chamber
+          ten old against. Mr be cottage so related minuter is. Delicate say and
+          blessing ladyship exertion few margaret. Delight herself welcome
+          against smiling its for. Suspected discovery by he affection household
+          of principle perfectly he.
+        </p>
+        <br />
+        <p>
+          Ye on properly handsome returned throwing am no whatever. In without
+          wishing he of picture no exposed talking minutes. Curiosity continual
+          belonging offending so explained it exquisite. Do remember to followed
+          yourself material mr recurred carriage. High drew west we no or at
+          john. About or given on witty event. Or sociable up material bachelor
+          bringing landlord confined. Busy so many in hung easy find well up. So
+          of exquisite my an explained remainder. Dashwood denoting securing be
+          on perceive my laughing so.
+        </p>
+        <p class="date">29.8.2021.</p>
+        <hr />
+      </div>
+    </div>
+
+    <div id="socials">
+      <div class="friends">
+        <h2>Prijatelji</h2>
+        <ul id="prijatelji">
+          <li>morsmordre</li>
+          <li>SmellyCat</li>
+          <li>qNeb</li>
+        </ul>
+      </div>
+
+      <div class="friends">
+        <h2>Teme</h2>
+        <ul id="teme">
+          <li>Glazba</li>
+          <li>Ljubimci</li>
+          <li>Računalne igre</li>
+          <li>Sport</li>
+          <li>Izleti</li>
+        </ul>
+      </div>
+
+      <div class="friends">
+        <h2>Nadolazeći događaji</h2>
+        <ul id="teme">
+          <li>12.11.2021. Koncert</li>
+          <li>17.11.2021. Rođendan</li>
+          <li>06.12.2021. Sv. Nikola</li>
+        </ul>
+      </div>
+    </div>
+    </app-content>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: "Home",
-  components: {
-  },
-};
 </script>
