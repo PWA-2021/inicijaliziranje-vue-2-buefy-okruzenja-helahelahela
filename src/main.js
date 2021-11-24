c358cda77a8c457f5a0227006ccbb8c44e0a68e4
helahelahela <hmakovic@tvz.hr>
2021-11-24 23:39:34 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,31 @@ import App from "./App.vue";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 Vue.use(Buefy);
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+new Vue({
+  el: '#app',
+  vuetify: new vuetify(),
+  
+})
+
+new Vue({
+  el: "#app",
+  vuetify: new vuetify(),
+  data: () => ({
+    items: [
+      { title: "Good-mood" },
+      { title: "Dosada" },
+      { title: "Busy Day" },
+    ],
+  }),
+})
