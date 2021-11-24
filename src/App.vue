223bef89214982f83dd9048e60ad8d6a4f4bf62d
helahelahela <hmakovic@tvz.hr>
2021-11-24 23:40:48 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <app-header></app-header>
-    <app-content></app-content>
     <router-view />
   </div>
 </template>
@@ -11,13 +9,4 @@
 </style>
 
 <script>
-import Header from "@/components/Header.vue";
-import Content from "@/components/Content.vue";
-
-export default {
-  components: {
-    "app-header": Header,
-    "app-content": Content,
-  },
-};
 </script>
