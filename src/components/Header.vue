cf038ab7efd2c1379950f1172f58547d53d783a5
helahelahela <hmakovic@tvz.hr>
2021-11-24 23:39:25 +0100

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,19 @@
 <template>
-    <div id="info">
-      <div id="user">
-        <div class="slika"></div>
-        <h1>Username</h1>
-      </div>
-
-      <div id="nav">
-        <ul>
-          <li><router-link to="/">Timeline</router-link></li>
-          <li><router-link to="/Calendar">Calendar</router-link></li>
-        </ul>
-      </div>
+  <div id="info">
+    <div id="user">
+      <div class="slika"></div>
+      <h1>Username</h1>
     </div>
+
+    <div id="nav">
+      <ul>
+        <li><router-link to="/">Timeline</router-link></li>
+        <li><router-link to="/Calendar">Calendar</router-link></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<style>
-</style>
+<style></style>
 
-<script>
-</script>
+<script></script>
