7d7cab6026d560610e14d83db13f2bccb41dbbe5
helahelahela <hmakovic@tvz.hr>
2021-11-24 23:39:47 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <app-content>
       <div id="display">
-      <h1>This is an about page.</h1>
+        <h1>This is an about page.</h1>
       </div>
     </app-content>
   </div>
