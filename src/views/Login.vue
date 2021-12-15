32cf0c6e0fd88d54c239c9860d4fa97a4b7416cd
helahelahela <hmakovic@tvz.hr>
2021-12-16 00:14:41 +0100

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,18 +4,18 @@
             <div id="login">
                 <h1>Bullet Journal</h1>
                 <form class="prijava">
-                <input type="email" class="log" placeholder="Enter E-mail" name="uname" required>
+                    <input type="email" class="log" placeholder="Enter E-mail" name="uname" id="email" required>
 
-                <input type="password" class="log" placeholder="Enter Password" name="psw" required>
+                    <input type="password" class="log" placeholder="Enter Password" name="psw" id="password" required>
 
-                <label for="checkbox" id="check"><input id="checkbox" type="checkbox" checked="checked" name="remember">Remember me</label>
+                    <label for="checkbox" id="check"><input id="checkbox" type="checkbox" checked="checked" name="remember">Remember me</label>
 
-                <a>Forgot your password?</a>
+                    <a>Forgot your password?</a>
 
-                <button type="submit" id="loginB"><router-link to="/"><h2 id="boja">Log In</h2></router-link></button>
-                <button id="regC"><h2 id="boja2">Register</h2></button>
-                <label>
-                </label>
+                    <button type="submit" id="loginB"><h2 id="boja">Log In</h2></button>
+                    <button id="regC"><h2 id="boja2">Register</h2></button>
+                    <label>
+                    </label>
                 </form>
             </div>
             <div id="slika">
@@ -29,4 +29,5 @@
 </style>
 
 <script>
+    
 </script>
