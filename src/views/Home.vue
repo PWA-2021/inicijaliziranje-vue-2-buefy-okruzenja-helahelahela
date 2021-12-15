ae4947d5397396cc3e19aa1a40d08ee16ffa457b
helahelahela <hmakovic@tvz.hr>
2021-12-15 01:56:50 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="home">
     <div id="info">
       <div id="user">
-        <img class="moon" src="../assets/drink.png">
+        <img class="moon" src="@/assets/img/drink.png">
         <h1>Username</h1>
       </div>
 
@@ -19,67 +19,26 @@
     </div>
 
     <div id="display">
-      <div class="article">
-        <h2>Sretan dan</h2>
-        <h3>Category: <span class="blue">Good-mood</span></h3>
-        <p>
-          May indulgence difficulty ham can put especially. Bringing remember
-          for supplied her why was confined. Middleton principle did she
-          procuring extensive believing add. Weather adapted prepare oh is
-          calling. These wrong of he which there smile to my front. He fruit oh
-          enjoy it of whose table. Cultivated occasional old her unpleasing
-          unpleasant. At as do be against pasture covered viewing started.
-          Enjoyed me settled mr respect no spirits civilly.
-        </p>
-        <br />
-        <p>
-          Material confined likewise it humanity raillery an unpacked as he.
-          Three chief merit no if. Now how her edward engage not horses. Oh
-          resolution he dissimilar precaution to comparison an. Matters engaged
-          between he of pursuit manners we moments. Merit gay end sight front.
-          Manor equal it on again ye folly by match. In so melancholy as an
-          sentiments simplicity connection. Far supply depart branch agreed old
-          get our.
-        </p>
-        <p class="date">9.10.2021.</p>
+        <ul class="posts">
+          <li v-for="post in posts" :key="post.title" class="article">
+            <h2>{{ post.title }}</h2><br>
+            <span>Category: <span class="blue">{{ post.category }}</span></span><br>
+            <p>{{ post.entry }}</p><br>
+            <p class="date">{{ post.date }}</p>
+            <!-- <div class="tooltip">{{ letter.alt }}</div> -->
+          </li>
+        </ul>
         <hr />
-      </div>
-
-      <div class="article">
-        <h2>Dosadan dan</h2>
-        <h3>Category: <span class="blue">Dosada</span></h3>
-        <p>
-          Prevailed sincerity behaviour to so do principle mr. As departure at
-          no propriety zealously my. On dear rent if girl view. First on smart
-          there he sense. Earnestly enjoyment her you resources. Brother chamber
-          ten old against. Mr be cottage so related minuter is. Delicate say and
-          blessing ladyship exertion few margaret. Delight herself welcome
-          against smiling its for. Suspected discovery by he affection household
-          of principle perfectly he.
-        </p>
-        <br />
-        <p>
-          Ye on properly handsome returned throwing am no whatever. In without
-          wishing he of picture no exposed talking minutes. Curiosity continual
-          belonging offending so explained it exquisite. Do remember to followed
-          yourself material mr recurred carriage. High drew west we no or at
-          john. About or given on witty event. Or sociable up material bachelor
-          bringing landlord confined. Busy so many in hung easy find well up. So
-          of exquisite my an explained remainder. Dashwood denoting securing be
-          on perceive my laughing so.
-        </p>
-        <p class="date">29.8.2021.</p>
-        <hr />
-      </div>
     </div>
 
     <div id="socials">
       <div class="friends">
         <h2>Prijatelji</h2>
         <ul id="prijatelji">
-          <li>morsmordre</li>
-          <li>SmellyCat</li>
-          <li>qNeb</li>
+          <li v-for="friend in friends" :key="friend.username">
+            <!-- <span>{{ friend.icon }}</span> -->
+            <h5 class="blue">{{ friend.username }}</h5>
+          </li>
         </ul>
       </div>
 
@@ -108,4 +67,25 @@
 </template>
 
 <script>
+  import json from "@/assets/data/DummyPosts.json";
+  import json2 from "@/assets/data/Friends.json";
+  export default {
+    name: "Home",
+    data: function() {
+      return {
+        posts: json.posts,
+        friends: json2.friends,
+      };
+    },
+    mounted: function() {
+    // console.log(this.posts);
+    console.log(this.friends);
+    },
+    // methods: {
+    // friendIcon: function(path) {
+    //   return require("@/" + path);
+    // },
+    // }
+  }
+
 </script>
