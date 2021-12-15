2af33fdc6c2e5ea0375001bda8781f3294a23737
helahelahela <hmakovic@tvz.hr>
2021-12-15 01:56:24 +0100

    Create Posts.vue

---
+++ src/components/Posts.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul class="posts">
+    <li v-for="post in posts" :key="post.title">
+      <span>{{ post.title }}</span>
+      <span>{{ post.category }}</span>
+      <span>{{ post.entry }}</span>
+      <span>{{ post.date }}</span>
+      <!-- <div class="tooltip">{{ letter.alt }}</div> -->
+    </li>
+  </ul>
+</template>
