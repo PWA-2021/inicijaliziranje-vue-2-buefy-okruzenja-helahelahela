0dc7baf3496be9b9213709f1b0ea36b54d56031c
helahelahela <hmakovic@tvz.hr>
2022-01-13 01:06:34 +0100

    Update App.vue
    Postovi na timeline-u i lista prijatelja se ispisuju iz json dokumenta, korisnici bi se trebali upisivati i izvlačiti iz firebase-a. Za sada je na vrhu stranice ostavljeno da se vidi kad je korisnik ulogiran, a kad ne. S obzirom na to da log in nije sasvim funkcionalan, na timeline se može doći i bez log in-a, no inače je osmišljeno da se timeline-u može pristupiti tek nakon što se korisnik ulogira.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <top-header></top-header>
     <router-view />
   </div>
 </template>
@@ -9,4 +10,8 @@
 </style>
 
 <script>
+import Top from "./components/Top.vue";
+export default {
+  components: {'top-header': Top}
+}
 </script>
