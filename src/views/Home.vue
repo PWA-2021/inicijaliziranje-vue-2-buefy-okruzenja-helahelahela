87b9044409bd519f979b66a78e516ccf36bd47fe
helahelahela <hmakovic@tvz.hr>
2022-02-10 15:50:33 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,14 @@
         <div id="sadrzaj">
             <div id="login">
                 <h1>Bullet Journal</h1>
-                <form class="prijava">
-                    <input type="email" class="log" placeholder="Enter E-mail" name="uname" id="email" required>
+                <form class="prijava" @submit.prevent="pressed">
+                    <div class="login">
+                    <input type="email" class="log" placeholder="Enter E-mail" name="email" id="email" v-model="email" required>
+                    </div>
 
-                    <input type="password" class="log" placeholder="Enter Password" name="psw" id="password" required>
+                    <div class="password">
+                    <input type="password" class="log" placeholder="Enter Password" name="password" id="password" v-model="password" required>
+                    </div>
 
                     <label for="checkbox" id="check"><input id="checkbox" type="checkbox" checked="checked" name="remember">Remember me</label>
 
@@ -16,7 +20,9 @@
                     <router-link to="/Register"><button id="regC"><h2 id="boja2">Register</h2></button></router-link>
                     <label>
                     </label>
+                    
                 </form>
+                <div class="error" v-if="error">{{error.message}}</div>
             </div>
             <div id="slika">
                 <router-link to="/Register"><button id="regB"><h2 id="boja2">Register</h2></button></router-link>
@@ -29,5 +35,28 @@
 </style>
 
 <script>
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+    export default {
+        data() {
+            return {
+                email: '',
+                password: '',
+                error: ''
+            }
+        },
+        methods: {
+            async pressed() {
+                try {
+                    const val = await firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+                    console.log(val);
+                    this.$router.replace({name: "Timeline"})
+                } catch(err) {
+                    console.log(err)
+                }
+            }
+        }
+    }
     
 </script>
