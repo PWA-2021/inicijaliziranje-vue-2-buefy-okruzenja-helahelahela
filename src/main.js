f2ecfcf10902ae397295a3091c430a31e6567ab3
helahelahela <hmakovic@tvz.hr>
2022-02-13 17:52:32 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,20 +5,20 @@ import "buefy/dist/buefy.css";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import axios from "axios";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAxIJk_HCYxYxyIcCRfYIXwx8qUPBHISqY",
-    authDomain: "journal-86463.firebaseapp.com",
-    projectId: "journal-86463",
-    storageBucket: "journal-86463.appspot.com",
-    messagingSenderId: "560017810713",
-    appId: "1:560017810713:web:cda0f408c11a1815b71188"
+  apiKey: "AIzaSyAxIJk_HCYxYxyIcCRfYIXwx8qUPBHISqY",
+  authDomain: "journal-86463.firebaseapp.com",
+  projectId: "journal-86463",
+  storageBucket: "journal-86463.appspot.com",
+  messagingSenderId: "560017810713",
+  appId: "1:560017810713:web:cda0f408c11a1815b71188",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -27,16 +27,16 @@ Vue.use(Buefy);
 Vue.use(vuetify);
 
 let app;
-firebase.auth().onAuthStateChanged(user=> {
+firebase.auth().onAuthStateChanged((user) => {
   console.log(user);
-  if(!app) {
+  if (!app) {
     app = new Vue({
       router,
       vuetify,
       render: (h) => h(App),
     }).$mount("#app");
   }
-})
+});
 
 // new Vue({
 //   router,
